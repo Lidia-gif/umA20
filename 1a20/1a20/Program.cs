b2058e9843118fb7a2106c290fac822b1b5ad842
Lidia-gif <lidiamaria.202069@gmail.com>
2024-11-08 13:00:32 +0000

    Add files via upload

---
+++ 1a20/1a20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+    //Exercício 1
+public class Num1a20
+{
+    public static void Main(string[] args)
+    {
+        for (int firstNum = 1; firstNum <= 20; firstNum++)
+        {
+            if (firstNum % 2 == 0)
+            {
+                Console.WriteLine(firstNum);
+            }
+        }
+    }
+}
